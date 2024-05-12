075fa654d0ed77debcf693b9c068c2938f20bb58
nizam323 <muhammadnizamuddin.sce@gmail.com>
2024-05-12 20:33:03 +0500

    working on random rating logic

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+let randomNum1 = Math.random() * 5 + 1;
+let pDR1 = randomNum1.toFixed(0);
+
+let randomNum2 = Math.random() * 5 + 1;
+let pDR2 = randomNum2.toFixed(0);
+
+let randomNum3 = Math.random() * 5 + 1;
+let pDR3 = randomNum3.toFixed(0);
+
+let randomNum4 = Math.random() * 5 + 1;
+let pDR4 = randomNum4.toFixed(0);
+
+let randomNum5 = Math.random() * 5 + 1;
+let pDR5 = randomNum5.toFixed(0);
+
+let randomNum6 = Math.random() * 5 + 1;
+let pDR6 = randomNum6.toFixed(0);
+
+let randomNum7 = Math.random() * 5 + 1;
+let pDR7 = randomNum7.toFixed(0);
+
+let randomNum8 = Math.random() * 5 + 1;
+let pDR8 = randomNum8.toFixed(0);
+
+let randomNum9 = Math.random() * 5 + 1;
+let pDR9 = randomNum9.toFixed(0);
+
+let randomNum10 = Math.random() * 5 + 1;
+let pDR10 = randomNum10.toFixed(0);
+
+
 let products = [
     {
         proName: "DNK Yellow Shoes",
@@ -6,38 +37,43 @@ let products = [
         proSalePrice: "$150.00",
         proPrice: "$120.00",
         proColor: "yes",
+        proRating: pDR1,
     },
     {
         proName: "DNK Blue Shoes",
         proImg: "images/product-2.jpg",
         proCategory: "Men",
-        proSalePrice: "",
+        proSalePrice: "no",
         proPrice: "$200.00 – $240.00",
         proColor: "",
+        proRating: pDR2,
     },
     {
         proName: "Dark Brown Jeans",
         proImg: "images/product-3.jpg",
         proCategory: "Men",
-        proSalePrice: "",
+        proSalePrice: "no",
         proPrice: "$150.00",
         proColor: "",
+        proRating: pDR3,
     },
     {
         proName: "Blue Denim Jeans",
         proImg: "images/product-4.jpg",
         proCategory: "Women",
-        proSalePrice: "",
+        proSalePrice: "no",
         proPrice: "$150.00",
         proColor: "",
+        proRating: pDR4,
     },
     {
         proName: "Basic Gray Jeans",
         proImg: "images/product-5.jpg",
         proCategory: "Women",
-        proSalePrice: "",
+        proSalePrice: "no",
         proPrice: "$150.00",
         proColor: "",
+        proRating: pDR5,
     },
     {
         proName: "Blue Denim Shorts",
@@ -46,38 +82,43 @@ let products = [
         proSalePrice: "$150.00",
         proPrice: "$130.00",
         proColor: "",
+        proRating: pDR6,
     },
     {
         proName: "Anchor Bracelet",
         proImg: "images/product-7.jpg",
         proCategory: "Accessories",
-        proSalePrice: "",
+        proSalePrice: "no",
         proPrice: "$150.00 – $180.00",
         proColor: "yes",
+        proRating: pDR7,
     },
     {
         proName: "Boho Bangle Bracelet",
         proImg: "images/product-8.jpg",
         proCategory: "Accessories",
-        proSalePrice: "",
+        proSalePrice: "no",
         proPrice: "$150.00 – $170.00",
         proColor: "yes",
+        proRating: pDR8,
     },
     {
         proName: "Light Brown Purse",
         proImg: "images/product-9.jpg",
         proCategory: "Accessories",
-        proSalePrice: "",
+        proSalePrice: "no",
         proPrice: "$150.00",
         proColor: "",
+        proRating: pDR9,
     },
     {
         proName: "Bright Red Bag",
         proImg: "images/product-10.jpg",
         proCategory: "Accessories",
-        proSalePrice: "",
+        proSalePrice: "no",
         proPrice: "$100.00 – $140.00",
         proColor: "yes",
+        proRating: pDR10,
     }
 ];
 
@@ -95,19 +136,15 @@ products.map((items) => {
     let proTxtParent = document.createElement("div");
     let proName = document.createElement("div");
     let proCategory = document.createElement("div");
-    let proSalePrice = document.createElement("strike");
     let proRealPrice = document.createElement("div");
     let proPrice = document.createElement("div");
     let proColor = document.createElement("div");
-    // let proColorPicker1 = document.createElement("div");
-    // let proColorPicker2 = document.createElement("div");
-    // let proColorPicker3 = document.createElement("div");
     let proRating = document.createElement("div");
-    let icon1 = document.createElement("i");
-    let icon2 = document.createElement("i");
-    let icon3 = document.createElement("i");
-    let icon4 = document.createElement("i");
-    let icon5 = document.createElement("i");
+    // let icon1 = document.createElement("i");
+    // let icon2 = document.createElement("i");
+    // let icon3 = document.createElement("i");
+    // let icon4 = document.createElement("i");
+    // let icon5 = document.createElement("i");
 
     // assigning css class to created elements
 
@@ -118,17 +155,13 @@ products.map((items) => {
     proCategory.classList.add("pro-category");
     proPrice.classList.add("pro-price");
     proColor.classList.add("pro-color");
-    // proColorPicker1.classList.add("pro-color-picker");
-    // proColorPicker2.classList.add("pro-color-picker");
-    // proColorPicker3.classList.add("pro-color-picker");
     proRating.classList.add("pro-rating");
     proRealPrice.style.display = "inline";
-    proRealPrice.style.padding = "0 0 0 10px";
-    icon1.classList.add("fa-solid", "fa-star");
-    icon2.classList.add("fa-solid", "fa-star");
-    icon3.classList.add("fa-solid", "fa-star-half-stroke");
-    icon4.classList.add("fa-regular", "fa-star");
-    icon5.classList.add("fa-regular", "fa-star");
+    // icon1.classList.add("fa-solid", "fa-star");
+    // icon2.classList.add("fa-solid", "fa-star");
+    // icon3.classList.add("fa-solid", "fa-star-half-stroke");
+    // icon4.classList.add("fa-regular", "fa-star");
+    // icon5.classList.add("fa-regular", "fa-star");
 
     // assinging text 
 
@@ -138,8 +171,6 @@ products.map((items) => {
     proName.innerText = items.proName;
     proCategory.innerText = items.proCategory;
     proRealPrice.innerText = items.proPrice;
-    proSalePrice.innerText = items.proSalePrice;
-
 
     // making cards through javascript
 
@@ -150,21 +181,17 @@ products.map((items) => {
     proTxtParent.appendChild(proName);
     proTxtParent.appendChild(proCategory);
     proTxtParent.appendChild(proPrice);
-    proPrice.appendChild(proSalePrice);
     proPrice.appendChild(proRealPrice)
     proTxtParent.appendChild(proColor);
-    // proColor.appendChild(proColorPicker1);
-    // proColor.appendChild(proColorPicker2);
-    // proColor.appendChild(proColorPicker3);
     proTxtParent.appendChild(proRating);
-    proRating.appendChild(icon1);
-    proRating.appendChild(icon2);
-    proRating.appendChild(icon3);
-    proRating.appendChild(icon4);
-    proRating.appendChild(icon5);
+    // proRating.appendChild(icon1);
+    // proRating.appendChild(icon2);
+    // proRating.appendChild(icon3);
+    // proRating.appendChild(icon4);
+    // proRating.appendChild(icon5);
 
 
-    if(items.proColor=="yes"){
+    if (items.proColor == "yes") {
         let proColorPicker1 = document.createElement("div");
         let proColorPicker2 = document.createElement("div");
         let proColorPicker3 = document.createElement("div");
@@ -175,5 +202,166 @@ products.map((items) => {
         proColor.appendChild(proColorPicker2);
         proColor.appendChild(proColorPicker3);
     }
+
+    if (items.proSalePrice !== "no") {
+        let proSalePrice = document.createElement("strike");
+        proSalePrice.innerText = items.proSalePrice;
+        proRealPrice.style.paddingLeft = "10px";
+        proPrice.insertAdjacentElement("afterbegin", proSalePrice)
+    }
+
+    if (items.proRating == 1) {
+
+        let icon1 = document.createElement("i");
+        let icon2 = document.createElement("i");
+        let icon3 = document.createElement("i");
+        let icon4 = document.createElement("i");
+        let icon5 = document.createElement("i");
+        
+        
+        icon1.classList.add("fa-solid", "fa-star");
+        // icon2.classList.add("fa-solid", "fa-star");
+        // icon3.classList.add("fa-solid", "fa-star-half-stroke");
+        // icon4.classList.add("fa-regular", "fa-star");
+        icon2.classList.add("fa-regular", "fa-star");
+        icon3.classList.add("fa-regular", "fa-star");
+        icon4.classList.add("fa-regular", "fa-star");
+        icon5.classList.add("fa-regular", "fa-star");
+        
+        
+        proRating.appendChild(icon1);
+        proRating.appendChild(icon2);
+        proRating.appendChild(icon3);
+        proRating.appendChild(icon4);
+        proRating.appendChild(icon5);
+
+
+    }
+    
+    if (items.proRating == 2) {
+
+        let icon1 = document.createElement("i");
+        let icon2 = document.createElement("i");
+        let icon3 = document.createElement("i");
+        let icon4 = document.createElement("i");
+        let icon5 = document.createElement("i");
+        
+        
+        icon1.classList.add("fa-solid", "fa-star");
+        icon2.classList.add("fa-solid", "fa-star");
+        // icon2.classList.add("fa-solid", "fa-star");
+        // icon3.classList.add("fa-solid", "fa-star-half-stroke");
+        // icon4.classList.add("fa-regular", "fa-star");
+        // icon2.classList.add("fa-regular", "fa-star");
+        icon3.classList.add("fa-regular", "fa-star");
+        icon4.classList.add("fa-regular", "fa-star");
+        icon5.classList.add("fa-regular", "fa-star");
+        
+        
+        proRating.appendChild(icon1);
+        proRating.appendChild(icon2);
+        proRating.appendChild(icon3);
+        proRating.appendChild(icon4);
+        proRating.appendChild(icon5);
+
+
+    }
+
+    
+    if (items.proRating == 3) {
+
+        let icon1 = document.createElement("i");
+        let icon2 = document.createElement("i");
+        let icon3 = document.createElement("i");
+        let icon4 = document.createElement("i");
+        let icon5 = document.createElement("i");
+        
+        
+        icon1.classList.add("fa-solid", "fa-star");
+        icon2.classList.add("fa-solid", "fa-star");
+        icon3.classList.add("fa-solid", "fa-star");
+        // icon2.classList.add("fa-solid", "fa-star");
+        // icon3.classList.add("fa-solid", "fa-star-half-stroke");
+        // icon4.classList.add("fa-regular", "fa-star");
+        // icon2.classList.add("fa-regular", "fa-star");
+        // icon3.classList.add("fa-regular", "fa-star");
+        icon4.classList.add("fa-regular", "fa-star");
+        icon5.classList.add("fa-regular", "fa-star");
+        
+        
+        proRating.appendChild(icon1);
+        proRating.appendChild(icon2);
+        proRating.appendChild(icon3);
+        proRating.appendChild(icon4);
+        proRating.appendChild(icon5);
+
+
+    }
+
+    
+    if (items.proRating == 4) {
+
+        let icon1 = document.createElement("i");
+        let icon2 = document.createElement("i");
+        let icon3 = document.createElement("i");
+        let icon4 = document.createElement("i");
+        let icon5 = document.createElement("i");
+        
+        
+        icon1.classList.add("fa-solid", "fa-star");
+        icon2.classList.add("fa-solid", "fa-star");
+        icon3.classList.add("fa-solid", "fa-star");
+        // icon4.classList.add("fa-solid", "fa-star");
+        // icon2.classList.add("fa-solid", "fa-star");
+        icon4.classList.add("fa-solid", "fa-star-half-stroke");
+        // icon4.classList.add("fa-regular", "fa-star");
+        // icon2.classList.add("fa-regular", "fa-star");
+        // icon3.classList.add("fa-regular", "fa-star");
+        // icon4.classList.add("fa-regular", "fa-star");
+        icon5.classList.add("fa-regular", "fa-star");
+        
+        
+        proRating.appendChild(icon1);
+        proRating.appendChild(icon2);
+        proRating.appendChild(icon3);
+        proRating.appendChild(icon4);
+        proRating.appendChild(icon5);
+
+
+    }
+    
+    if (items.proRating == 5) {
+
+        let icon1 = document.createElement("i");
+        let icon2 = document.createElement("i");
+        let icon3 = document.createElement("i");
+        let icon4 = document.createElement("i");
+        let icon5 = document.createElement("i");
+        
+        
+        icon1.classList.add("fa-solid", "fa-star");
+        icon2.classList.add("fa-solid", "fa-star");
+        icon3.classList.add("fa-solid", "fa-star");
+        icon4.classList.add("fa-solid", "fa-star");
+        icon5.classList.add("fa-solid", "fa-star");
+        // icon2.classList.add("fa-solid", "fa-star");
+        // icon3.classList.add("fa-solid", "fa-star-half-stroke");
+        // icon4.classList.add("fa-regular", "fa-star");
+        // icon2.classList.add("fa-regular", "fa-star");
+        // icon3.classList.add("fa-regular", "fa-star");
+        // icon4.class/List.add("fa-regular", "fa-star");
+        // icon5.classList.add("fa-regular", "fa-star");
+        
+        
+        proRating.appendChild(icon1);
+        proRating.appendChild(icon2);
+        proRating.appendChild(icon3);
+        proRating.appendChild(icon4);
+        proRating.appendChild(icon5);
+
+
+    }
+
+    console.log(items.proRating)
 
 });
